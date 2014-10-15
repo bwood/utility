0a57d5290bbe22992859ee3abd84b80157cccd04
Brian Wood <bwood@berkeley.edu>
2014-10-15 15:50:30 -0700

    Change variables so they won't clash with environment

--- openberkeley-remotes.sh
+++ openberkeley-remotes.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-PROTOCOL='git@'            # change if you use https:// or something else
-DOMAIN='github-bwood.com'  # change depending on your use of ssh-config
-USER='bwood'               # this is your github username
-# the $USER uri's below assume you have these forks on GitHub
-git remote add -f openberkeley_admin-upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/openberkeley_admin.git 
-git remote add -f openberkeley_admin $PROTOCOL$DOMAIN:$USER/openberkeley_admin.git 
-git remote add -f openberkeley_base $PROTOCOL$DOMAIN:$USER/openberkeley_base.git 
-git remote add -f openberkeley_base-upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/openberkeley_base.git 
-git remote add -f openberkeley_faq $PROTOCOL$DOMAIN:$USER/openberkeley_faq.git 
-git remote add -f openberkeley_faq-upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/openberkeley_faq.git 
-git remote add -f openberkeley_news $PROTOCOL$DOMAIN:$USER/openberkeley_news.git 
-git remote add -f openberkeley_news-upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/openberkeley_news.git 
-git remote add -f openberkeley_pages $PROTOCOL$DOMAIN:$USER/openberkeley_pages.git 
-git remote add -f openberkeley_pages-upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/openberkeley_pages.git 
-git remote add -f openberkeley_starter $PROTOCOL$DOMAIN:$USER/openberkeley_starter.git 
-git remote add -f openberkeley_starter-upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/openberkeley_starter.git 
-git remote add -f ucberkeley_cas $PROTOCOL$DOMAIN:$USER/ucberkeley_cas-7-test.git 
-git remote add -f ucberkeley_cas-upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/ucberkeley_cas-7.git 
-git remote add -f upstream $PROTOCOL$DOMAIN:ucb-ist-drupal/openberkeley-drops-7.git 
+GIT_PROTOCOL='git@'            # change if you use https:// or something else
+GIT_DOMAIN='github-bwood.com'  # change depending on your use of ssh-config
+GIT_USER='bwood'               # this is your github username
+# the $GIT_USER uri's below assume you have these forks on GitHub
+git remote add -f openberkeley_admin-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley_admin.git 
+git remote add -f openberkeley_admin $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/openberkeley_admin.git 
+git remote add -f openberkeley_base $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/openberkeley_base.git 
+git remote add -f openberkeley_base-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley_base.git 
+git remote add -f openberkeley_core_override $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/openberkeley_core_override.git 
+git remote add -f openberkeley_core_override-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley_core_override.git 
+git remote add -f openberkeley_faq $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/openberkeley_faq.git 
+git remote add -f openberkeley_faq-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley_faq.git 
+git remote add -f openberkeley_news $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/openberkeley_news.git 
+git remote add -f openberkeley_news-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley_news.git 
+git remote add -f openberkeley_pages $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/openberkeley_pages.git 
+git remote add -f openberkeley_pages-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley_pages.git 
+git remote add -f openberkeley_starter $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/openberkeley_starter.git 
+git remote add -f openberkeley_starter-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley_starter.git 
+git remote add -f ucberkeley_cas $GIT_PROTOCOL$GIT_DOMAIN:$GIT_USER/ucberkeley_cas-7-test.git 
+git remote add -f ucberkeley_cas-upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/ucberkeley_cas-7.git 
+git remote add -f upstream $GIT_PROTOCOL$GIT_DOMAIN:ucb-ist-drupal/openberkeley-drops-7.git 
