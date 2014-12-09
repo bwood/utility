05e5d95259f802826787dee036bb91dee97a062c
Brian Wood <bwood@berkeley.edu>
2014-12-08 17:05:34 -0800

    Download and untar experiments

--- doit.php
+++ doit.php
@@ -1,4 +1,174 @@
 <?php
+/**
+ * Copy remote file over HTTP one small chunk at a time.
+ *
+ * @param $infile The full URL to the remote file
+ * @param $outfile The path where to save the file
+ */
+function copyfile_chunked($infile, $outfile) {
+  $chunksize = 10 * (1024 * 1024); // 10 Megs
+
+  /**
+   * parse_url breaks a part a URL into it's parts, i.e. host, path,
+   * query string, etc.
+   */
+  $parts = parse_url($infile);
+  $i_handle = fsockopen($parts['host'], 80, $errstr, $errcode, 5);
+  $o_handle = fopen($outfile, 'wb');
+
+  if ($i_handle == false || $o_handle == false) {
+    return false;
+  }
+
+  if (!empty($parts['query'])) {
+    $parts['path'] .= '?' . $parts['query'];
+  }
+
+  /**
+   * Send the request to the server for the file
+   */
+  $request = "GET {$parts['path']} HTTP/1.1\r\n";
+  $request .= "Host: {$parts['host']}\r\n";
+  $request .= "User-Agent: Mozilla/5.0\r\n";
+  $request .= "Keep-Alive: 115\r\n";
+  $request .= "Connection: keep-alive\r\n\r\n";
+  fwrite($i_handle, $request);
+
+  /**
+   * Now read the headers from the remote server. We'll need
+   * to get the content length.
+   */
+  $headers = array();
+  while(!feof($i_handle)) {
+    $line = fgets($i_handle);
+    if ($line == "\r\n") break;
+    $headers[] = $line;
+  }
+
+  /**
+   * Look for the Content-Length header, and get the size
+   * of the remote file.
+   */
+  $length = 0;
+  foreach($headers as $header) {
+    if (stripos($header, 'Content-Length:') === 0) {
+      $length = (int)str_replace('Content-Length: ', '', $header);
+      break;
+    }
+  }
+
+  /**
+   * Start reading in the remote file, and writing it to the
+   * local file one chunk at a time.
+   */
+  $cnt = 0;
+  while(!feof($i_handle)) {
+    $buf = '';
+    $buf = fread($i_handle, $chunksize);
+    $bytes = fwrite($o_handle, $buf);
+    if ($bytes == false) {
+      return false;
+    }
+    $cnt += $bytes;
+
+    /**
+     * We're done reading when we've reached the conent length
+     */
+    if ($cnt >= $length) break;
+  }
+
+  fclose($i_handle);
+  fclose($o_handle);
+  return $cnt;
+}
+
+
+$infile = $argv[1];
+$outfile = $argv[2];
+copyfile_chunked($infile, $outfile);
+// Success
+
+
+exit();
+
+/*
+ * Download and unarchive pantheon backup with exec
+*/
+$url = $argv[1];
+$archive = $argv[2];
+
+$file_gz = '/tmp/' . $archive;
+//$file_tar = preg_replace("/\.gz$/", "", $file_gz);
+
+set_time_limit(0);
+$fp = fopen ($file_gz, 'w+');
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+curl_setopt($ch, CURLOPT_FILE, $fp); // write curl response to file
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_exec($ch); // get curl response
+curl_close($ch);
+fclose($fp);
+
+exit;
+
+$cd_files_mount_cmd = "cd /tmp/test";
+$tar_cmd = "tar --strip-components 1 -z -x -f $file_gz";
+exec("$cd_files_mount_cmd;$tar_cmd");
+
+exit;
+
+/*
+ * Download and unarchive pantheon backup PharData
+*/
+$url = $argv[1];
+$remote_file = $argv[2];
+$file_gz = '/tmp/' . $remote_file;
+$file_tar = preg_replace("/\.gz$/", "", $file_gz);
+/*
+set_time_limit(0);
+$fp = fopen ($tmp_file, 'w+');
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+curl_setopt($ch, CURLOPT_FILE, $fp); // write curl response to file
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_exec($ch); // get curl response
+curl_close($ch);
+fclose($fp);
+*/
+
+// decompress from gz
+try {
+  $gzip_file = new PharData($file_gz);
+  //$tar_file = $gzip_file->decompress(); // creates /path/to/my.tar
+  $gzip_file->decompress(); //now $file_tar exists
+} catch (Exception $e) {
+  print "Error unzipping.\n";
+  print $e->getMessage();
+}
+
+try {
+  //$tar_file->extractTo('/tmp');
+  $tar_file = new PharData($file_tar);
+  $tar_file->extractTo('/Users/bwood/tmp/test2');
+} catch (Exception $e) {
+  print "Error untarring.\n";
+  print $e->getMessage();
+}
+
+/*
+ PharData buggy?  Always get
+ Extraction from phar "/private/tmp/openucb-469-ebill-bw_dev_2014-12-04T18-48-06_UTC_files.tar" failed:
+ Cannot extract "files_dev/ebillscreerm /tmp/openucb-469-ebill-bw_dev_2014-12-04T18-48-06_UTC_files.tar
+ */
+
+exit;
+// couldn't find a php way to strip files_dev/ when extracting
+if (!rename('/tmp/files_dev', '/tmp/files')) {
+  print "Error: Failed copying to /tmp/files.\n";
+}
+
+exit;
 
 // ring the terminal bell?
 echo "\007";
